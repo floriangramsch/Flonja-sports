1bfea3dbbe1c560382b0d16db53ef966a352695d
Florian Gramsch <florigram@gmail.com>
2024-10-10 15:53:31 +0200

    manifest and github_workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,35 @@ export default defineNuxtConfig({
     head: {
       title: "Flonja Sports",
       meta: [
-        { name: "theme-color", content: "#D5BDAF" },
-        { name: "description", content: "Planer für Fiona" },
+        { name: "theme-color", content: "#27272a" },
+        { name: "msapplication-TileColor", content: "#da532c" },
+        { name: "description", content: "Fitness App für Se Bebis." },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+        },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
       ],
       script: [
         {
