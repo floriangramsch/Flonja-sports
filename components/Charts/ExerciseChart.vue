1042007c7cd6593e43bffe261748189ee5a2ad18
Florian Gramsch <florigram@gmail.com>
2024-12-02 13:00:42 +0100

    better charts

--- components/Charts/ExerciseChart.vue
+++ components/Charts/ExerciseChart.vue
@@ -6,7 +6,8 @@ const props = defineProps<{
   user: string;
 }>();
 
-console.log(props.data);
+const type = ref<"volume" | "sets">("sets");
+
 props.data.map((t: any) => {
   console.log(t.weight, t.reps, t.weight * t.reps);
 });
@@ -14,7 +15,104 @@ props.data.map((t: any) => {
 const chartCanvas = ref<HTMLCanvasElement | null>(null);
 let chartInstance: Chart<"line", { x: number; y: number }[]> | null = null;
 
-const createChart = () => {
+const getPointRadius = (reps: number) => {
+  if (reps <= 6) return 5;
+  if (reps <= 8) return 7;
+  if (reps <= 10) return 9;
+  if (reps <= 12) return 11;
+  return 13;
+};
+
+const createTopSetsChart = () => {
+  if (!chartCanvas.value) return;
+
+  // Falls bereits ein Chart existiert, diesen zerstören
+  if (chartInstance) {
+    chartInstance.destroy();
+  }
+
+  // Neuen Chart erstellen
+  chartInstance = new Chart(chartCanvas.value, {
+    type: "line",
+    data: {
+      labels: props.data.map((_: any, index: number) => index + 1),
+
+      datasets: [
+        {
+          label: props.user,
+
+          data: props.data?.map((d: any) => d.weight),
+          // data: props.data?.map((d: any) => d.weight * d.reps),
+          // data:
+          //   props.data?.map((d) => ({
+          //     x: d.start,
+          //     y: d.weight * d.reps,
+          //     pointRadius: getPointRadius(d.reps),
+          //   })) || [],
+          backgroundColor: "#1e293b",
+          borderColor: "#1e293b",
+          borderWidth: 1,
+          // pointRadius: 3,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          type: "category",
+          title: {
+            display: true,
+            text: "Index",
+          },
+        },
+        y: {
+          beginAtZero: false,
+          title: {
+            display: true,
+            text: "Weigth",
+          },
+        },
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: (context) => {
+              const index = context.dataIndex;
+              const data = props.data[index];
+              return `Weight: ${data.weight}, Reps: ${data.reps}`;
+            },
+          },
+        },
+      },
+      elements: {
+        point: {
+          radius: (context) => {
+            const index = context.dataIndex;
+            const data = props.data[index];
+            return getPointRadius(data.reps);
+          },
+        },
+      },
+      // plugins: {
+      //   legend: {
+      //     position: "top",
+      //   },
+      //   tooltip: {
+      //     callbacks: {
+      //       label: (context) =>
+      //         `${context.dataset.label}: ${
+      //           (context.raw as { y: number }).y || 0
+      //         }`,
+      //     },
+      //   },
+      // },
+    },
+  });
+};
+
+const createVolumeChart = () => {
   if (!chartCanvas.value) return;
 
   // Falls bereits ein Chart existiert, diesen zerstören
@@ -33,15 +131,10 @@ const createChart = () => {
           label: props.user,
 
           data: props.data?.map((d: any) => d.weight * d.reps),
-          // data:
-          //   props.data?.map((d) => ({
-          //     x: d.start,
-          //     y: d.weight * d.reps,
-          //   })) || [],
           backgroundColor: "#1e293b",
           borderColor: "#1e293b",
           borderWidth: 1,
-          pointRadius: 3,
+          pointRadius: 5,
         },
       ],
     },
@@ -75,29 +168,26 @@ const createChart = () => {
           },
         },
       },
-      // plugins: {
-      //   legend: {
-      //     position: "top",
-      //   },
-      //   tooltip: {
-      //     callbacks: {
-      //       label: (context) =>
-      //         `${context.dataset.label}: ${
-      //           (context.raw as { y: number }).y || 0
-      //         }`,
-      //     },
-      //   },
-      // },
     },
   });
 };
 
-onMounted(createChart);
+const switchChart = () => {
+  if (type.value === "sets") type.value = "volume";
+  else if (type.value === "volume") type.value = "sets";
+};
+
+onMounted(createTopSetsChart);
+
+watch(type, (newVal) => {
+  if (newVal === "sets") createTopSetsChart();
+  else if (newVal === "volume") createVolumeChart();
+});
 
 watch(
   () => props.data,
   () => {
-    createChart();
+    createTopSetsChart();
   },
   { deep: true },
 );
@@ -105,6 +195,7 @@ watch(
 
 <template>
   <div class="sm:h-90 relative h-64 w-full">
+    <button @click="switchChart"><i class="fa-solid fa-cat" /></button>
     <canvas class="min-w-full" ref="chartCanvas" />
   </div>
 </template>
