4462b5caf08590d80294d34f19bc7e0b5c6ac759
Florian Gramsch <mail@floxsite.de>
2026-06-19 12:22:20 +0200

    fix: slightly better position for search bar

--- components/Filter/FilterExercises.vue
+++ components/Filter/FilterExercises.vue
@@ -4,19 +4,17 @@
     ref="openRef"
     class="fa-solid fa-magnifying-glass flex h-10 items-center rounded-full bg-sonja-bg-darker px-4 text-sonja-text shadow"
   />
-  <DropdownSlideTransition>
-    <input
-      v-if="isOpen"
-      v-outside="close"
-      class="absolute mr-10 translate-x-5 translate-y-12 rounded-md bg-sonja-text p-1 text-center text-2xl text-sonja-akz2 shadow-lg focus:outline-none focus:ring-2 focus:ring-sonja-akz"
-      :class="class"
-      ref="input"
-      v-model="search"
-      @input="filter"
-      @keydown="closeInput"
-      @blur="handleBlur"
-    />
-  </DropdownSlideTransition>
+  <input
+    v-if="isOpen"
+    v-outside="close"
+    class="absolute left-1/2 top-1/2 -translate-x-full translate-y-full rounded-md bg-sonja-text p-1 text-center text-2xl text-sonja-akz2 shadow-lg focus:outline-none focus:ring-2 focus:ring-sonja-akz"
+    :class="class"
+    ref="input"
+    v-model="search"
+    @input="filter"
+    @keydown="closeInput"
+    @blur="handleBlur"
+  />
 </template>
 
 <script setup lang="ts">
